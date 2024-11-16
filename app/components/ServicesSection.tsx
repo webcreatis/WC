7b7eddf76aee11544d72a426a004991254adc924
webcreatis <contact@webcreatis.fr>
2024-11-16 17:55:59 +0400

    WEBCREATIS > Add custom cursor + display services pics on hover

--- app/components/ServicesSection.tsx
+++ app/components/ServicesSection.tsx
@@ -1,6 +1,65 @@
+"use client";
+
 import { Laptop } from "lucide-react";
+import { useCallback, useEffect, useRef } from "react";
+import brandingBoardPics from "../assets/images/Branding-board-webcreatis.webp";
+import kakemonoPics from "../assets/images/Envergure-Kakemono.webp";
+import canvaPics from "../assets/images/logo-canva.webp";
+import prestaPics from "../assets/images/presta-webcreatis.webp";
+import websitePics from "../assets/images/site-seaintense.webp";
+import socialNetworkPics from "../assets/images/social-network-webcreatis.webp";
 
 export default function ServicesSection() {
+  const cursorRef = useRef<HTMLDivElement>(null);
+
+  const cursorPosition = useCallback((e: MouseEvent) => {
+    const x = e.clientX;
+    const y = e.clientY;
+    const cursor = cursorRef.current;
+
+    if (cursor) {
+      cursor.style.top = `${y}px`;
+      cursor.style.left = `${x}px`;
+    }
+  }, []);
+
+  useEffect(() => {
+    const boxes = document.querySelectorAll(".box");
+
+    boxes.forEach((elem) => {
+      elem.addEventListener("mouseenter", function () {
+        const att = elem.getAttribute("data-image");
+
+        const cursor = cursorRef.current;
+        if (cursor) {
+          cursor.classList.add("cursorGrow");
+          cursor.style.background = `url(${att})`;
+          cursor.style.backgroundPosition = "center";
+          cursor.style.backgroundSize = "contain";
+          cursor.style.backgroundRepeat = "no-repeat";
+        }
+      });
+      elem.addEventListener("mouseout", function () {
+        const cursor = cursorRef.current;
+        if (cursor) {
+          cursor.classList.remove("cursorGrow");
+          cursor.style.background = "#000000";
+        }
+      });
+    });
+
+    window.addEventListener("mousemove", cursorPosition);
+
+    // Cleanup des événements pour éviter les fuites de mémoire
+    return () => {
+      boxes.forEach((elem) => {
+        elem.removeEventListener("mouseenter", () => {});
+        elem.removeEventListener("mouseout", () => {});
+      });
+      window.removeEventListener("mousemove", cursorPosition);
+    };
+  }, [cursorPosition]);
+
   return (
     <section className="services w-full h-auto flex-col justify-start items-start">
       <div
@@ -19,7 +78,10 @@ export default function ServicesSection() {
       </div>
       <div className="w-full h-auto pb-32">
         <ul className="w-3/4 m-auto">
-          <li className="flex w-full border-b-[1px] border-white mt-5">
+          <li
+            className="flex w-full border-b-[1px] border-white mt-5 box"
+            data-image={websitePics.src}
+          >
             <h3 className="inline-block text-6xl font-jaapokki uppercase text-green">
               website
             </h3>
@@ -35,7 +97,10 @@ export default function ServicesSection() {
               </span>
             </div>
           </li>
-          <li className="flex w-full border-b-[1px] border-white mt-5">
+          <li
+            className="flex w-full border-b-[1px] border-white mt-5 box"
+            data-image={brandingBoardPics.src}
+          >
             <h3 className="inline-block text-6xl font-jaapokki uppercase text-green">
               webdesign
             </h3>
@@ -54,7 +119,10 @@ export default function ServicesSection() {
               </span>
             </div>
           </li>
-          <li className="flex w-full border-b-[1px] border-white mt-5">
+          <li
+            className="flex w-full border-b-[1px] border-white mt-5 box"
+            data-image={kakemonoPics.src}
+          >
             <h3 className="inline-block text-6xl font-jaapokki uppercase text-green">
               design & print
             </h3>
@@ -68,12 +136,12 @@ export default function ServicesSection() {
               <span className="font-jaapokki uppercase text-xl text-white after:content-['|'] after:ml-0.5 after:text-green">
                 kakemono
               </span>
-              <span className="font-jaapokki uppercase text-xl text-white">
-                kakemono
-              </span>
             </div>
           </li>
-          <li className="flex w-full border-b-[1px] border-white mt-5">
+          <li
+            className="flex w-full border-b-[1px] border-white mt-5 box"
+            data-image={socialNetworkPics.src}
+          >
             <h3 className="inline-block text-6xl font-jaapokki uppercase text-green">
               social network
             </h3>
@@ -92,29 +160,23 @@ export default function ServicesSection() {
               </span>
             </div>
           </li>
-          <li className="flex w-full border-b-[1px] border-white mt-5">
+          <li
+            className="flex w-full border-b-[1px] border-white mt-5 box"
+            data-image={canvaPics.src}
+          >
             <h3 className="inline-block text-6xl font-jaapokki uppercase text-green">
               formation
             </h3>
             <div className="flex justify-start items-end gap-1 ml-10">
-              <span className="font-jaapokki uppercase text-xl text-white after:content-['|'] after:ml-0.5 after:text-green">
-                react
-              </span>
-              <span className="font-jaapokki uppercase text-xl text-white after:content-['|'] after:ml-0.5 after:text-green">
-                flutter
-              </span>
-              <span className="font-jaapokki uppercase text-xl text-white after:content-['|'] after:ml-0.5 after:text-green">
-                js
-              </span>
-              <span className="font-jaapokki uppercase text-xl text-white after:content-['|'] after:ml-0.5 after:text-green">
-                html-css
-              </span>
               <span className="font-jaapokki uppercase text-xl text-white">
                 canva
               </span>
             </div>
           </li>
-          <li className="flex w-full border-b-[1px] border-white mt-5">
+          <li
+            className="flex w-full border-b-[1px] border-white mt-5 box"
+            data-image={prestaPics.src}
+          >
             <h3 className="inline-block text-6xl font-jaapokki uppercase text-green">
               presta
             </h3>
@@ -135,6 +197,10 @@ export default function ServicesSection() {
           </li>
         </ul>
       </div>
+      <div
+        ref={cursorRef}
+        className="cursor w-[20px] h-[20px] rounded-full bg-green border-2 border-white border-solid fixed pointer-events-none bg-contain bg-center"
+      ></div>
     </section>
   );
 }
