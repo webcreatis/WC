ef63c3d2d0f2e559d0a12f5079a470825a020e86
webcreatis <contact@webcreatis.fr>
2024-11-22 11:51:23 +0400

    WEBCREATIS > wip create Footer - layout

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,9 +2,12 @@ import Meteo from "../ui/Meteo";
 
 export default function Footer() {
   return (
-    <footer className="bg-black w-full h-[500px] flex flex-col">
-      <div className="flex">
-        <Meteo />
+    <footer className=" bg-black w-full h-auto flex">
+      <div className="container mx-auto border-2 border-solid border-red-700">
+        <div className="pt-24 grid grid-cols-2 gap-4">
+          <Meteo />
+          <Meteo />
+        </div>
       </div>
     </footer>
   );
