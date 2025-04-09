713ba806bbeef06f72c3cee6f7609bd0a4da50ab
webcreatis <contact@webcreatis.fr>
2025-04-09 10:03:25 +0400

    WEBCREATIS > Contact >  global Refacto + create Contact Card component

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,93 +1,46 @@
-"use client";
-import { Clock9, Instagram, Mail, MapPin, Phone } from "lucide-react";
-import Link from "next/link";
-import ContactForm from "../ui/ContactForm";
+import ContactCard from "../ui/ContactCard";
 import CustomCursor from "../ui/CustomCursor";
 import TextAnimated from "../ui/TextAnimated";
 
 export default function ContactPage() {
   return (
     <section className="contact w-full flex flex-col text-white font-raleway pt-40">
-      <TextAnimated title="contact" />
-      <div className="min-h-screen py-12 px-4 sm:px-6 lg:px-8">
-        <div className="max-w-7xl mx-auto">
-          <h1 className="text-5xl font-bold text-center text-white mb-10 font-jaapokki">
-            Demande de devis
-          </h1>
-
-          <div className="bg-white rounded-lg shadow-xl overflow-hidden lg:grid lg:grid-cols-2">
-            <div className="bg-darkGreen text-white p-8 lg:p-12">
-              <div className="max-w-lg mx-auto">
-                <h2 className="text-2xl font-bold mb-6">Contactez-nous</h2>
-
-                <div className="space-y-8">
-                  <div className="flex items-start gap-5">
-                    <MapPin color="#ffffff" />
-                    <div>
-                      <h3 className="font-semibold mb-1">Camp de base</h3>
-                      <p className="text-indigo-100">
-                        {" "}
-                        La Possession - île de la Réunion
-                      </p>
-                    </div>
-                  </div>
-
-                  <div className="flex items-start gap-5">
-                    <Phone color="#ffffff" />
-                    <div>
-                      <h3 className="font-semibold mb-1">Téléphone</h3>
-                      <a
-                        data-link="link"
-                        href="tel:+262693514439"
-                        className="buttonAction"
-                      >
-                        <span className="font-raleway">06.93.51.44.39</span>
-                      </a>
-                    </div>
-                  </div>
-
-                  <div className="flex items-start gap-5">
-                    <Mail color="#ffffff" />
-                    <div>
-                      <h3 className="font-semibold mb-1">Email</h3>
-                      <a
-                        data-link="link"
-                        href="mailto:contact.webcreatis@gmail.com"
-                        className="buttonAction"
-                      >
-                        <span className="font-raleway">
-                          contact.webcreatis@gmail.com
-                        </span>
-                      </a>
-                    </div>
-                  </div>
-
-                  <div className="flex items-start gap-5">
-                    <Clock9 color="#ffffff" />
-                    <div>
-                      <h3 className="font-semibold mb-1">Horaires</h3>
-                      <p className="text-indigo-100">Lun-Ven: 9h00 - 18h00</p>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="mt-12">
-                  <h3 className="font-semibold mb-4">Suivez-nous</h3>
-                  <div className="flex space-x-4">
-                    <Link
-                      href="https://www.instagram.com/webcreatis/"
-                      className="text-indigo-100 hover:text-white buttonAction"
-                      data-link="link"
-                    >
-                      <span className="sr-only">Instagram</span>
-                      <Instagram color="#ffffff" />
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <ContactForm />
-          </div>
+      <TextAnimated title="contact us" />
+      <div className="h-auto py-12 px-4 sm:px-6 lg:px-8 pb-40">
+        <h1 className="w-full flex justify-center xs:text-xl xs:pb-10 xs:pl-3 mobile:text-center md:text-3xl md:leading-10 lg:p-20 lg:text-5xl lg:leading-relaxed xl:text-7xl xl:pb-28">
+          Vous avez un projet digital ou une idée de collaboration ? Parlons-en.
+        </h1>
+        <div className="w-full flex flex-col items-center gap-5">
+          <ContactCard
+            title="Camp de base, la Réunion"
+            link="https://www.google.com/maps/place//data=!4m2!3m1!1s0x218287c7a88b4291:0x1e0c6bfe54c406e6?sa=X&ved=1t:8290&ictx=111"
+            textLink="view on maps"
+          />
+          <ContactCard
+            title="mail"
+            link="mailto:contact.webcreatis@gmail.com"
+            textLink="contact.webcreatis@gmail.com"
+          />
+          <ContactCard
+            title="tél Patrick - site internet"
+            link="tel:+262693514439"
+            textLink="06 93 51 44 39"
+          />
+          <ContactCard
+            title="tél Patricia - com digitale"
+            link="tel:+262693844876"
+            textLink="06 93 84 48 76"
+          />
+          <ContactCard
+            title="social network"
+            link="https://www.instagram.com/webcreatis/"
+            textLink="instagram"
+          />
+          <ContactCard
+            title="planifier une visio"
+            link="https://www.instagram.com/webcreatis/"
+            textLink="calendly"
+          />
         </div>
       </div>
       <CustomCursor />
---
+++ app/ui/ContactCard.tsx
@@ -0,0 +1,43 @@
+import { ArrowUpRight } from "lucide-react";
+import Link from "next/link";
+
+type ContactCardProps = {
+  title: string;
+  link: string;
+  textLink: string;
+};
+
+export default function ContactCard({
+  title,
+  link,
+  textLink,
+}: ContactCardProps) {
+  return (
+    <div className="relative flex w-[90%] md:w-[70%] m-auto h-auto justify-between border-2 border-white min-[320px]:flex-col laptop:flex-row laptop:w-full lg:w-[90%] xl:w-[70%]">
+      <p className="w-full font-semibold text-2xl p-5 xs:text-lg md:flex-1 md:text-3xl border-b-2 md:border-b-2 md:border-r-0 md:p-7 laptop:border-b-0 laptop:border-r-2 laptop:text-left xl:p-14 uppercase text-center">
+        {title}
+      </p>
+
+      <Link
+        data-link="link"
+        href={link}
+        className="relative buttonAction w-full md:flex-1 text-2xl md:text-3xl flex items-center overflow-hidden group"
+      >
+        {/* background anim hover */}
+        <span className="absolute inset-0 bg-corail scale-x-0 origin-left transition-transform duration-500 ease-in-out group-hover:scale-x-100 z-0" />
+
+        {/* content */}
+        <div className="w-20 md:w-28 border-r-2 h-full flex justify-center items-center z-10 xs:hidden md:flex">
+          <ArrowUpRight
+            color="#ffffff"
+            className="h-full md:w-[38px] laptop:w-[40px] xl:w-[60px]"
+          />
+        </div>
+
+        <p className="flex-1 text-xl xs:text-base xs:pl-2 xs:pr-2 xs:pt-5 xs:pb-5 md:text-xl p-5 md:p-7 lg:text-2xl xl:text-3xl z-10 text-center md:text-left">
+          {textLink}
+        </p>
+      </Link>
+    </div>
+  );
+}
