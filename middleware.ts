449dab86ead6609ed96196117dea88576331dd1c
webcreatis <contact@webcreatis.fr>
2025-05-25 11:36:42 +0400

    WEBCREATIS > WIP Fix redirection

--- middleware.ts
+++ middleware.ts
@@ -5,15 +5,24 @@ import { NextResponse } from "next/server";
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
+  console.log("pathname--------------", pathname);
+
   // Liste des anciennes URLs à rediriger vers la racine
   const legacyRoutes = [
     "/web-design/",
     "/gestion-de-vos-reseaux-sociaux/",
     "/creation-de-logo/",
     "/design-print/",
+    "/web-design",
+    "/gestion-de-vos-reseaux-sociaux",
+    "/creation-de-logo",
+    "/design-print",
+    "/testimonials/",
+    "/testimonials",
   ];
 
   if (legacyRoutes.includes(pathname)) {
+    console.log("ok in legacy routes--------------");
     const url = request.nextUrl.clone();
     url.pathname = "/"; // redirige vers la page d’accueil
     return NextResponse.redirect(url, 308);
