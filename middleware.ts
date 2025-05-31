486ef8133cb45a50e2d24d9fea305fc6b24abebb
webcreatis <contact@webcreatis.fr>
2025-05-31 17:29:41 +0400

    WEBCREATIS > Fix redirection .re .fr

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,15 @@
-// middleware.ts
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
+  const { pathname, hostname } = request.nextUrl;
+
+  // Redirection globale de .re vers .fr
+  if (hostname.endsWith(".re")) {
+    const url = request.nextUrl.clone();
+    url.hostname = hostname.replace(".re", ".fr");
+    return NextResponse.redirect(url, 308); // redirection permanente
+  }
 
   // Liste des anciennes URLs à rediriger vers la racine
   const legacyRoutes = [
@@ -22,7 +28,6 @@ export function middleware(request: NextRequest) {
   ];
 
   if (legacyRoutes.includes(pathname)) {
-    console.log("ok in legacy routes--------------");
     const url = request.nextUrl.clone();
     url.pathname = "/"; // redirige vers la page d’accueil
     return NextResponse.redirect(url, 308);
