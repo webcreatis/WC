c67e9435ba602d30ed8e79c5234f139e2fea35d7
webcreatis <contact@webcreatis.fr>
2025-06-01 23:37:30 +0400

    WEBCREATIS > refacto seo for hero section

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -44,13 +44,16 @@ export default function HeroSection() {
           </span>
         </div>
       </h2>
+      <h1 className="text-base font-bold text-white text-center pt-10 pb-5 font-raleway md:text-lg lg:text-2xl xl:text-3xl">
+        Création de site internet à La Réunion – Webdesign UX/UI, Mentoring &
+        Accompagnement Canva
+      </h1>
       <div className="hero-subtitle w-3/4 caption-timeline text-white pt-10 xs:p-7 xs:w-full tablet:w-3/4 lg:w-[65%] animate-fadeIn transition-all duration-1000 ease-in-out">
-        <h1 className="text-sm text-center tablet:text-sm ipadPro:text-lg laptop:text-center">
-          {`Nous sommes deux passionnés du web & du digital basés à La Réunion.
-          Nous vous accompagnons dans la création de votre site internet, de votre identité visuelle et dans le développement de vos projets en ligne.
-          Nous proposons du mentoring pour les développeurs web en formation, des audits de sites pour vous aider à améliorer l’existant, et des accompagnements canva pour gagner en autonomie et créer vos design.
-          Nous sommes là pour vous guider, avec simplicité, écoute et créativité !`}
-        </h1>
+        <p className="text-sm text-center ipadPro:text-lg laptop:text-center xl:text-xl">
+          {`Webcreatis, agence digitale à La Réunion, vous accompagne dans votre stratégie de communication digitale à travers la création ou la refonte de sites internet professionnels sur mesure.
+           Nous allions UX/UI design, développement web, identité visuelle et SEO pour bâtir une présence en ligne cohérente et performante. Mentoring développeurs, 
+           accompagnement Canva et aide Kap Numérik de la Région Réunion disponibles pour concrétiser votre projet.`}
+        </p>
       </div>
       <MarqueeHeader />
       <div className="mt-14 flex flex-col w-full items-center gap-5 mb-10 s:gap-2 mobile:gap-3 md:flex-row md:px-10 laptop:mt-14 laptop:gap-1 laptop:w-3/5 lg:w-[30%] lg:pt-10">
@@ -58,6 +61,7 @@ export default function HeroSection() {
           href="https://calendly.com/contact-webcreatis/30min"
           data-link="link"
           tabIndex={0}
+          role="button"
           className="buttonAction z-20 flex justify-center w-[200px] h-[3rem] p-2 text-xs items-center uppercase cursor-pointer rounded-md bg-corail mobile:text-sm mobile:w-2/3 mobile:m-auto laptop:w-[150px] laptop:xs:h-[3.5em] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-corail"
         >
           RDV en ligne
@@ -66,6 +70,7 @@ export default function HeroSection() {
           href="/contact"
           data-link="link"
           tabIndex={0}
+          role="button"
           className="buttonAction z-20 flex justify-center w-[200px] h-[3rem] p-2 text-xs items-center uppercase cursor-pointer rounded-md bg-white mobile:text-sm mobile:w-2/3 mobile:m-auto laptop:w-[150px] laptop:xs:h-[3.5em] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-corail"
         >
           contact
@@ -74,13 +79,14 @@ export default function HeroSection() {
           href="#projects"
           data-link="link"
           tabIndex={0}
+          role="button"
           className="buttonAction z-20 flex justify-center w-[200px] h-[3rem] p-2 text-xs items-center uppercase cursor-pointer rounded-md bg-white mobile:text-sm mobile:w-2/3 mobile:m-auto laptop:w-[150px] laptop:xs:h-[3.5em] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-corail"
         >
           Réalisations
         </Link>
       </div>
 
-      <div className="flex w-full justify-between items-center p-2 mobile:mt-10 md:mt-20 lg:mt-28 lg:p-5">
+      <div className="flex w-full justify-between items-center p-2 mobile:mt-10 md:mt-20 lg:mt-24 lg:p-5">
         <CrossIcon />
         <CrossIcon />
         <span className="uppercase text-white font-raleway text-xs md:text-base lg:text-2xl">
