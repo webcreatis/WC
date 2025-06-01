7cfe9e0e7674057a9a1f9a4921336ae8b3edd5a1
webcreatis <contact@webcreatis.fr>
2025-06-01 11:12:55 +0400

    WEBCREATIS > refacto width section for mention,plan and politique pages

--- app/mentions-legales/page.tsx
+++ app/mentions-legales/page.tsx
@@ -41,96 +41,101 @@ export const metadata: Metadata = {
 export default function MentionsLegales() {
   return (
     <section className="politique px-5 space-y-6 w-full font-raleway text-white pb-20 md:px-10">
-      <h1 className="text-2xl font-bold mb-6 font-jaapokki pt-52 pb-5 w-full md:pb-10 md:text-3xl xl:text-5xl">
-        Mentions Légales
-      </h1>
-      <h2 className="text-xl font-semibold xl:text-3xl">
-        Édition du site Internet
-      </h2>
-      <p className="text-base md:text-lg xl:text-xl">
-        Le site Webcreatis, accessible aux adresses{" "}
-        <strong>webcreatis.re</strong> et <strong>webcreatis.fr</strong>, sont
-        édité par Patrick et Patricia Perroudon.
-      </p>
+      <div className="lg:w-1/2 lg:m-auto">
+        <h1 className="text-3xl font-bold mb-6 font-jaapokki pt-52 pb-5 w-full md:pb-10 md:text-4xl lg:text-5xl">
+          Mentions Légales
+        </h1>
+        <h2 className="text-xl font-semibold xl:text-3xl">
+          Édition du site Internet
+        </h2>
+        <p className="text-base md:text-lg xl:text-xl">
+          Le site Webcreatis, accessible aux adresses{" "}
+          <strong>webcreatis.re</strong> et <strong>webcreatis.fr</strong>, sont
+          édité par Patrick et Patricia Perroudon.
+        </p>
 
-      <h2 className="text-xl font-semibold xl:text-3xl">
-        Directeur de la publication
-      </h2>
-      <p className="text-base md:text-lg xl:text-xl">Patrick Perroudon</p>
-      <p className="text-base md:text-lg xl:text-xl">Patricia Perroudon</p>
+        <h2 className="text-xl font-semibold xl:text-3xl">
+          Directeur de la publication
+        </h2>
+        <p className="text-base md:text-lg xl:text-xl">Patrick Perroudon</p>
+        <p className="text-base md:text-lg xl:text-xl">Patricia Perroudon</p>
 
-      <h2 className="text-xl font-semibold xl:text-3xl">
-        Salariés indépendants portés par la société :
-      </h2>
-      <p className="text-base md:text-lg xl:text-xl">Réunion Portage</p>
-      <p className="text-base md:text-lg xl:text-xl">97419 La Possession</p>
-      <p className="text-base md:text-lg xl:text-xl">SIRET : 78846591200038</p>
-      <p className="text-base md:text-lg xl:text-xl">
-        Forme juridique : SARL unipersonnelle
-      </p>
-      <p className="text-base md:text-lg xl:text-xl">
-        Contact :{" "}
-        <a
-          href="mailto:contact.webcreatis@gmail.com"
-          className="text-base md:text-lg xl:text-xl"
-        >
-          contact.webcreatis@gmail.com
-        </a>
-      </p>
-      <p className="text-base md:text-lg xl:text-xl">
-        Téléphone : 06 93 51 44 39
-      </p>
+        <h2 className="text-xl font-semibold xl:text-3xl">
+          Salariés indépendants portés par la société :
+        </h2>
+        <p className="text-base md:text-lg xl:text-xl">Réunion Portage</p>
+        <p className="text-base md:text-lg xl:text-xl">97419 La Possession</p>
+        <p className="text-base md:text-lg xl:text-xl">
+          SIRET : 78846591200038
+        </p>
+        <p className="text-base md:text-lg xl:text-xl">
+          Forme juridique : SARL unipersonnelle
+        </p>
+        <p className="text-base md:text-lg xl:text-xl">
+          Contact :{" "}
+          <a
+            href="mailto:contact.webcreatis@gmail.com"
+            className="text-base md:text-lg xl:text-xl"
+          >
+            contact.webcreatis@gmail.com
+          </a>
+        </p>
+        <p className="text-base md:text-lg xl:text-xl">
+          Téléphone : 06 93 51 44 39
+        </p>
 
-      <h2 className="text-xl font-semibold xl:text-3xl">
-        Hébergeur du site Internet
-      </h2>
-      <p className="text-base md:text-lg xl:text-xl">
-        Le site Webcreatis est hébergé par Vercel. <br></br>La société Vercel
-        Inc. est située au 440 N Barranca Ave, PMB 4133, Covina, CA 91723,
-        États-Unis. <br></br>Pour toute question, vous pouvez les contacter par
-        téléphone au +1 (951) 383-6898.
-      </p>
+        <h2 className="text-xl font-semibold xl:text-3xl">
+          Hébergeur du site Internet
+        </h2>
+        <p className="text-base md:text-lg xl:text-xl">
+          Le site Webcreatis est hébergé par Vercel. <br></br>La société Vercel
+          Inc. est située au 440 N Barranca Ave, PMB 4133, Covina, CA 91723,
+          États-Unis. <br></br>Pour toute question, vous pouvez les contacter
+          par téléphone au +1 (951) 383-6898.
+        </p>
 
-      <h2 className="text-xl font-semibold xl:text-3xl">
-        Respect de la propriété intellectuelle
-      </h2>
-      <p className="text-base md:text-lg xl:text-xl">
-        Toutes les marques, photographies, textes, commentaires, illustrations,
-        images animées ou non, séquences vidéo, sons, ainsi que toutes les
-        applications informatiques qui pourraient être utilisées pour faire
-        fonctionner le Site et plus généralement tous les éléments reproduits ou
-        utilisés sur le Site sont protégés par les lois en vigueur au titre de
-        la propriété intellectuelle.
-      </p>
-      <p className="text-base md:text-lg xl:text-xl">
-        Ils sont la propriété pleine et entière de l’Editeur ou de ses
-        partenaires, sauf mentions particulières. Toute reproduction,
-        représentation, utilisation ou adaptation, sous quelque forme que ce
-        soit, de tout ou partie de ces éléments, y compris les applications
-        informatiques, sans l’accord préalable et écrit de l’Editeur, sont
-        strictement interdites. Le fait pour l’Editeur de ne pas engager de
-        procédure dès la prise de connaissance de ces utilisations non
-        autorisées ne vaut pas acceptation desdites utilisations et renonciation
-        aux poursuites.
-      </p>
-      <p className="text-base md:text-lg xl:text-xl">
-        Seule l’utilisation pour un usage privé dans un cercle de famille est
-        autorisée et toute autre utilisation est constitutive de contrefaçon
-        et/ou d’atteinte aux droits voisins, sanctionnées par le Code de la
-        propriété intellectuelle.
-      </p>
-      <p className="text-base md:text-lg xl:text-xl">
-        La reprise de tout ou partie de ce contenu nécessite l’autorisation
-        préalable de l’Editeur ou du titulaire des droits sur ce contenu.
-      </p>
+        <h2 className="text-xl font-semibold xl:text-3xl">
+          Respect de la propriété intellectuelle
+        </h2>
+        <p className="text-base md:text-lg xl:text-xl">
+          Toutes les marques, photographies, textes, commentaires,
+          illustrations, images animées ou non, séquences vidéo, sons, ainsi que
+          toutes les applications informatiques qui pourraient être utilisées
+          pour faire fonctionner le Site et plus généralement tous les éléments
+          reproduits ou utilisés sur le Site sont protégés par les lois en
+          vigueur au titre de la propriété intellectuelle.
+        </p>
+        <p className="text-base md:text-lg xl:text-xl">
+          Ils sont la propriété pleine et entière de l’Editeur ou de ses
+          partenaires, sauf mentions particulières. Toute reproduction,
+          représentation, utilisation ou adaptation, sous quelque forme que ce
+          soit, de tout ou partie de ces éléments, y compris les applications
+          informatiques, sans l’accord préalable et écrit de l’Editeur, sont
+          strictement interdites. Le fait pour l’Editeur de ne pas engager de
+          procédure dès la prise de connaissance de ces utilisations non
+          autorisées ne vaut pas acceptation desdites utilisations et
+          renonciation aux poursuites.
+        </p>
+        <p className="text-base md:text-lg xl:text-xl">
+          Seule l’utilisation pour un usage privé dans un cercle de famille est
+          autorisée et toute autre utilisation est constitutive de contrefaçon
+          et/ou d’atteinte aux droits voisins, sanctionnées par le Code de la
+          propriété intellectuelle.
+        </p>
+        <p className="text-base md:text-lg xl:text-xl">
+          La reprise de tout ou partie de ce contenu nécessite l’autorisation
+          préalable de l’Editeur ou du titulaire des droits sur ce contenu.
+        </p>
 
-      <h2 className="text-xl font-semibold xl:text-3xl">Liens hypertextes</h2>
-      <p className="text-base md:text-lg xl:text-xl">
-        Le Site peut contenir des liens hypertexte donnant accès à d’autres
-        sites web édités et gérés par des tiers et non par l’Editeur. L’Editeur
-        ne pourra être tenu responsable directement ou indirectement dans le cas
-        où lesdits sites tiers ne respecteraient pas les dispositions légales.
-      </p>
+        <h2 className="text-xl font-semibold xl:text-3xl">Liens hypertextes</h2>
+        <p className="text-base md:text-lg xl:text-xl">
+          Le Site peut contenir des liens hypertexte donnant accès à d’autres
+          sites web édités et gérés par des tiers et non par l’Editeur.
+          L’Editeur ne pourra être tenu responsable directement ou indirectement
+          dans le cas où lesdits sites tiers ne respecteraient pas les
+          dispositions légales.
+        </p>
+      </div>
       <CustomCursor />
     </section>
   );
--- app/politique-de-confidentialite/page.tsx
+++ app/politique-de-confidentialite/page.tsx
@@ -41,125 +41,128 @@ export const metadata: Metadata = {
 export default function PolitiqueConfidentialite() {
   return (
     <section className="politique px-5 space-y-6 w-full font-raleway text-white pb-20 md:px-10">
-      <h1 className="text-2xl font-bold mb-6 font-jaapokki pt-52 pb-5 w-full md:pb-10 md:text-3xl xl:text-5xl">
-        Politique de confidentialité
-      </h1>
+      <div className="lg:w-1/2 lg:m-auto">
+        <h1 className="text-3xl font-bold mb-6 font-jaapokki pt-52 pb-5 w-full md:pb-10 md:text-4xl lg:text-5xl">
+          Politique de confidentialité
+        </h1>
 
-      <p className="text-base md:text-lg xl:text-xl">
-        Politique de confidentialité de WebCreatis.re et .fr
-      </p>
+        <p className="text-base md:text-lg xl:text-xl">
+          Politique de confidentialité de WebCreatis.re et .fr
+        </p>
 
-      <p className="text-base md:text-lg xl:text-xl">
-        Chez WebCreatis, nous accordons une grande importance à la protection de
-        vos données personnelles. Cette politique de confidentialité explique
-        comment nous recueillons, utilisons et protégeons les informations que
-        vous nous fournissez lorsque vous utilisez notre site Web.
-      </p>
+        <p className="text-base md:text-lg xl:text-xl">
+          Chez WebCreatis, nous accordons une grande importance à la protection
+          de vos données personnelles. Cette politique de confidentialité
+          explique comment nous recueillons, utilisons et protégeons les
+          informations que vous nous fournissez lorsque vous utilisez notre site
+          Web.
+        </p>
 
-      <h2 className="text-xl font-semibold xl:text-3xl">
-        Collecte et utilisation des données personnelles
-      </h2>
-      <p className="text-base md:text-lg xl:text-xl">
-        Lorsque vous visitez notre site Web, nous pouvons collecter certaines
-        informations personnelles vous concernant.
-        <br />
-        Nous utilisons vos informations personnelles pour vous fournir les
-        services que vous avez demandés, pour répondre à vos questions et pour
-        vous envoyer des communications pertinentes concernant nos services. Vos
-        informations peuvent également être utilisées à des fins internes telles
-        que l’analyse statistique, l’amélioration de nos services et la
-        personnalisation de votre expérience sur notre site Web.
-      </p>
+        <h2 className="text-xl font-semibold xl:text-3xl">
+          Collecte et utilisation des données personnelles
+        </h2>
+        <p className="text-base md:text-lg xl:text-xl">
+          Lorsque vous visitez notre site Web, nous pouvons collecter certaines
+          informations personnelles vous concernant.
+          <br />
+          Nous utilisons vos informations personnelles pour vous fournir les
+          services que vous avez demandés, pour répondre à vos questions et pour
+          vous envoyer des communications pertinentes concernant nos services.
+          Vos informations peuvent également être utilisées à des fins internes
+          telles que l’analyse statistique, l’amélioration de nos services et la
+          personnalisation de votre expérience sur notre site Web.
+        </p>
 
-      <h2 className="text-xl font-semibold xl:text-3xl">
-        Protection des données
-      </h2>
-      <p className="text-base md:text-lg xl:text-xl">
-        Nous prenons toutes les mesures nécessaires pour assurer la sécurité et
-        la confidentialité de vos données personnelles. Nous utilisons des
-        mesures de sécurité appropriées pour protéger vos informations contre
-        tout accès, divulgation, altération ou destruction non autorisés.
-      </p>
+        <h2 className="text-xl font-semibold xl:text-3xl">
+          Protection des données
+        </h2>
+        <p className="text-base md:text-lg xl:text-xl">
+          Nous prenons toutes les mesures nécessaires pour assurer la sécurité
+          et la confidentialité de vos données personnelles. Nous utilisons des
+          mesures de sécurité appropriées pour protéger vos informations contre
+          tout accès, divulgation, altération ou destruction non autorisés.
+        </p>
 
-      <h2 className="text-xl font-semibold xl:text-3xl">
-        Partage des données personnelles
-      </h2>
-      <div className="text-base md:text-lg xl:text-xl">
-        Nous ne vendons, ne louons ni ne partageons vos données personnelles
-        avec des tiers, sauf dans les cas suivants :
-        <ul className="list-disc list-inside mt-2 ml-4">
-          <li>
-            Si vous avez donné votre consentement explicite pour le partage de
-            vos informations.
-          </li>
-          <li>
-            Si cela est nécessaire pour répondre à une obligation légale ou pour
-            protéger nos droits, notre propriété ou notre sécurité, ainsi que
-            ceux de nos utilisateurs ou du public.
-          </li>
-        </ul>
+        <h2 className="text-xl font-semibold xl:text-3xl">
+          Partage des données personnelles
+        </h2>
+        <div className="text-base md:text-lg xl:text-xl">
+          Nous ne vendons, ne louons ni ne partageons vos données personnelles
+          avec des tiers, sauf dans les cas suivants :
+          <ul className="list-disc list-inside mt-2 ml-4">
+            <li>
+              Si vous avez donné votre consentement explicite pour le partage de
+              vos informations.
+            </li>
+            <li>
+              Si cela est nécessaire pour répondre à une obligation légale ou
+              pour protéger nos droits, notre propriété ou notre sécurité, ainsi
+              que ceux de nos utilisateurs ou du public.
+            </li>
+          </ul>
+        </div>
+
+        <h2 className="text-xl font-semibold xl:text-3xl">
+          Liens vers des sites tiers
+        </h2>
+        <p className="text-base md:text-lg xl:text-xl">
+          Notre site Web peut contenir des liens vers des sites Web de tiers.
+          Veuillez noter que nous ne sommes pas responsables de la collecte, de
+          l’utilisation ou de la divulgation des informations, des pratiques de
+          confidentialité ou du contenu de ces sites Web. Nous vous encourageons
+          à lire les politiques de confidentialité de ces sites tiers avant de
+          fournir vos informations personnelles.
+        </p>
+
+        <h2 className="text-xl font-semibold xl:text-3xl">
+          Conservation des données
+        </h2>
+        <p className="text-base md:text-lg xl:text-xl">
+          Nous ne conservons vos données personnelles que le temps nécessaire
+          pour atteindre les finalités pour lesquelles elles ont été collectées,
+          sauf si la loi nous oblige à les conserver plus longtemps.
+        </p>
+
+        <h2 className="text-xl font-semibold xl:text-3xl">Vos droits</h2>
+        <p className="text-base md:text-lg xl:text-xl">
+          Vous avez le droit d’accéder, de corriger, de mettre à jour ou de
+          supprimer vos informations personnelles que nous détenons. Si vous
+          souhaitez exercer ces droits ou si vous avez des questions concernant
+          notre politique de confidentialité, veuillez nous contacter en
+          utilisant les informations fournies ci-dessous.
+        </p>
+
+        <h2 className="text-xl font-semibold xl:text-3xl">
+          Modifications de la politique de confidentialité
+        </h2>
+        <p className="text-base md:text-lg xl:text-xl">
+          Nous nous réservons le droit de modifier cette politique de
+          confidentialité à tout moment. Toute modification sera publiée sur
+          cette page avec une date de mise à jour. Nous vous encourageons à
+          consulter régulièrement cette page pour rester informé des éventuelles
+          modifications.
+        </p>
+
+        <h2 className="text-xl font-semibold xl:text-3xl">Nous contacter</h2>
+        <p className="mb-2">
+          Si vous avez des questions, des préoccupations ou des demandes
+          concernant cette politique de confidentialité, veuillez nous contacter
+          à l’adresse suivante :
+        </p>
+        <p className="text-base md:text-lg xl:text-xl">
+          <strong>WebCreatis</strong>
+          <br />
+          Camp de base : La Réunion
+          <br />
+          Téléphone : 06 93 51 44 39
+          <br />
+          E-mail : contact.webcreatis@gmail.com
+        </p>
+
+        <p className="text-base text-green md:text-lg xl:text-xl">
+          Dernière mise à jour : Dimanche 25 Mai 2025
+        </p>
       </div>
-
-      <h2 className="text-xl font-semibold xl:text-3xl">
-        Liens vers des sites tiers
-      </h2>
-      <p className="text-base md:text-lg xl:text-xl">
-        Notre site Web peut contenir des liens vers des sites Web de tiers.
-        Veuillez noter que nous ne sommes pas responsables de la collecte, de
-        l’utilisation ou de la divulgation des informations, des pratiques de
-        confidentialité ou du contenu de ces sites Web. Nous vous encourageons à
-        lire les politiques de confidentialité de ces sites tiers avant de
-        fournir vos informations personnelles.
-      </p>
-
-      <h2 className="text-xl font-semibold xl:text-3xl">
-        Conservation des données
-      </h2>
-      <p className="text-base md:text-lg xl:text-xl">
-        Nous ne conservons vos données personnelles que le temps nécessaire pour
-        atteindre les finalités pour lesquelles elles ont été collectées, sauf
-        si la loi nous oblige à les conserver plus longtemps.
-      </p>
-
-      <h2 className="text-xl font-semibold xl:text-3xl">Vos droits</h2>
-      <p className="text-base md:text-lg xl:text-xl">
-        Vous avez le droit d’accéder, de corriger, de mettre à jour ou de
-        supprimer vos informations personnelles que nous détenons. Si vous
-        souhaitez exercer ces droits ou si vous avez des questions concernant
-        notre politique de confidentialité, veuillez nous contacter en utilisant
-        les informations fournies ci-dessous.
-      </p>
-
-      <h2 className="text-xl font-semibold xl:text-3xl">
-        Modifications de la politique de confidentialité
-      </h2>
-      <p className="text-base md:text-lg xl:text-xl">
-        Nous nous réservons le droit de modifier cette politique de
-        confidentialité à tout moment. Toute modification sera publiée sur cette
-        page avec une date de mise à jour. Nous vous encourageons à consulter
-        régulièrement cette page pour rester informé des éventuelles
-        modifications.
-      </p>
-
-      <h2 className="text-xl font-semibold xl:text-3xl">Nous contacter</h2>
-      <p className="mb-2">
-        Si vous avez des questions, des préoccupations ou des demandes
-        concernant cette politique de confidentialité, veuillez nous contacter à
-        l’adresse suivante :
-      </p>
-      <p className="text-base md:text-lg xl:text-xl">
-        <strong>WebCreatis</strong>
-        <br />
-        Camp de base : La Réunion
-        <br />
-        Téléphone : 06 93 51 44 39
-        <br />
-        E-mail : contact.webcreatis@gmail.com
-      </p>
-
-      <p className="text-base text-green md:text-lg xl:text-xl">
-        Dernière mise à jour : Dimanche 25 Mai 2025
-      </p>
       <CustomCursor />
     </section>
   );
